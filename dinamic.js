398d3bf961379bfc764b0b421c225879e6c6153f
Carmine <kappa85t@hotmail.it>
2018-11-23 11:57:41 +0100

    created a dinamically

--- dinamic.js
+++ dinamic.js
@@ -2,18 +2,24 @@ let nav = document.createElement('nav');
 let aHome = document.createElement('a');
 // let aGroup = aHome*5;
 
-let aHamburg = document.createElement('a');
-let aRom = document.createElement('a');
-let aBarcellona = document.createElement('a');
-let aNewYork = document.createElement('a');
-let aMoskow = document.createElement('a');
+// let aHamburg = document.createElement('a');
+// let aRom = document.createElement('a');
+// let aBarcellona = document.createElement('a');
+// let aNewYork = document.createElement('a');
+// let aMoskow = document.createElement('a');
 
-let elCities = [aHamburg, aRom, aBarcellona, aNewYork, aMoskow];
+// let elCities = [aHamburg, aRom, aBarcellona, aNewYork, aMoskow];
+
+let arrayAElements = [];
+for(n=0; n<5;n++){
+  arrayAElements.push(document.createElement('a'));
+}
+
 let cities =['Hamburg','Rome','Barcellona','New York','Moscow'];
 
-for (let i=0; i< elCities.length; i++){
-   nav.appendChild(elCities[i]);
-   elCities[i].innerHTML = cities[i];
+for (let i=0; i< arrayAElements.length; i++){
+   nav.appendChild(arrayAElements[i]);
+   arrayAElements[i].innerHTML = cities[i];
 }
 
 
